0639c90af6d9f1de4146bc16dd42fe9b32b0a07a
SaFaUU <thulk283@gmail.com>
2022-11-09 01:22:17 +0600

    first commit

---
+++ src/contexts/AuthProvider/AuthProvider.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyDuU75zhJrkU5WMXqSNUt5COY_Jp4TVKgU",
+    authDomain: "service-review-f334a.firebaseapp.com",
+    projectId: "service-review-f334a",
+    storageBucket: "service-review-f334a.appspot.com",
+    messagingSenderId: "652047888596",
+    appId: "1:652047888596:web:4a0431a1c29a8549e9cb41"
+};
+
+// Initialize Firebase
+export const app = initializeApp(firebaseConfig);
